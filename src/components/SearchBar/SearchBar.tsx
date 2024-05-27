d4b7692976a7adc0cf7f7c663675b029216b306c
sofia tselen <sofiatselenx@gmail.com>
2024-05-27 22:49:35 +0300

    add search bar

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,37 @@
+import { Field, Form, Formik } from "formik";
+import css from "./SearchBar.module.css";
+import toast from "react-hot-toast";
+import { FC } from "react";
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: FC<SearchBarProps> = ({ onSearch }) => {
+  const handleSubmit = (values: { query: string }) => {
+    if (!values.query.trim()) {
+      toast.error("Write anything😉");
+      return;
+    }
+    onSearch(values.query);
+  };
+  return (
+    <header className={css.header}>
+      <Formik initialValues={{ query: "" }} onSubmit={handleSubmit}>
+        <Form className={css.form}>
+          <Field
+            className={css.input}
+            type="text"
+            name="query"
+            placeholder="Search images and photos"
+          ></Field>
+          <button className={css.btnSearch} type="submit">
+            Search
+          </button>
+        </Form>
+      </Formik>
+    </header>
+  );
+};
+
+export default SearchBar;
